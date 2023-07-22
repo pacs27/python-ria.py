0d7eb510bfc762da7961fa7a4ffc19e8fbb39b11
pacs27 <pacopuig1995@gmail.com>
2023-07-22 12:21:08 -0700

    Fixed a bug in the fetch request due to API changes

--- ria/callback/http_get_callback.py
+++ ria/callback/http_get_callback.py
@@ -1,16 +1,19 @@
 import json
 
 from ria._typing import HttpGetRequestRet
-from requests import get
+from requests import request
 
 
 def http_get_callback(url: str) -> HttpGetRequestRet:
-    response = get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    payload = {}
+    response = request("GET", url, headers=headers, data=payload)
+
     if response.status_code == 200:
         return {"status": response.status_code, "data": json.loads(response.text)}
     else:
         if response.status_code == 404:
-            response_google = get("http://www.google.com")
+            response_google = request("GET","http://www.google.com")
             if response_google.status_code == 404:
                 raise Exception(
                     "No es posible conectar con la API. Compruebe que está conectado a internet."
